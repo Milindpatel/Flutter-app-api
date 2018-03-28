19ecd6bbc8382f243a86ee2d9c6c8fb79e3055ba
Milindpatel <milindpatel3959@gmail.com>
2018-03-27 22:37:24 -0400

    added functionality for login as well as for memberinfo

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ mongoose.connect(config.database);
 require('./config/passport')(passport);
 var apiRoutes = express.Router();
 
+//for signup
 apiRoutes.post('/signup', function(req,res){
     if(!req.body.name || !req.body.password){
         res.json({success : false, msg: 'Please pass name and password.'});
@@ -45,6 +46,64 @@ apiRoutes.post('/signup', function(req,res){
         })
     }
 });
+
+//for login
+apiRoutes.post('/authenticate', function(req,res){
+    User.findOne({
+        name: req.body.name
+    }, function(err, user){
+        if(err) throw err;
+
+        if(!user){
+            return res.status(403).send({success: false, msg : 'Authentication failed.User not found.'});
+        }else{
+            user.comparePassword(req.body.password, function(err, isMatch){
+                if(isMatch && !err){
+                    var token = jwt.encode(user, config.secret);
+
+                    res.json({success : true, token : 'JWT ' + token});
+                }else{
+                    return res.status(403).send({success: false, msg: 'Authentication Failed. Wrong Password.'})
+                }
+            })
+        }
+    })
+});
+
+apiRoutes.get('/memberinfo', passport.authenticate('jwt', {session: false}), function(req, res){
+    var token = getToken(req.headers);
+    if(token){
+        var decoded = jwt.decode( token, config.secret);
+        User.findOne({
+            name: decoded.name
+        }, function(err, user){
+            if(err) throw err;
+
+            if(!user){
+                return res.status(403).send({ success: false, msg: 'Authentication failed.User not found.' })
+            }else{
+                return res.json({success: true, msg: 'Welcome in the member area ' + user.name + '!'});
+            }
+        })
+    }else{
+        return res.status(403).send({ success: false, msg: 'No token provided.' })
+    }
+});
+
+getToken = function(headers){
+    if(headers && headers.authorization){
+        var parted = headers.authorization.split(' ');
+    
+        if(parted.length == 2){
+            return parted[1];
+        }else{
+            return null;
+        }
+    }
+    else{
+        return null;
+    }
+}
 
 app.use('/api', apiRoutes);
 
